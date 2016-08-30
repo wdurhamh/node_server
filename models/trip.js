1f5fbde8dc2340f16ec2546f5232174c7555204b
root <root@ip-172-31-35-115.us-west-2.compute.internal>
2016-08-30 23:50:39 +0000

    simplified tripd datamodel

--- models/trip.js
+++ models/trip.js
@@ -1,12 +1,7 @@
 var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 var Trip = new Schema({
-	date:String,
-	time:String,
-	temp:String,
-	weather:String,
-	water_conditions:String,
-	lake_id:Number
+	name:String
 });
 
 module.exports = mongoose.model('Trip', Trip);
